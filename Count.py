93d4505b451b0459bce80fef1658ee31cb6b2003
anushab05 <anusha50898@gmail.com>
2018-05-02 00:40:55 +0530

    Read file, count words and sentences

---
+++ Count.py
@@ -0,0 +1,9 @@
+import re
+
+FileObject = open("TestDocument.txt", "r")
+FileText = FileObject.read()
+Number_Of_Words = len(re.findall("[a-zA-Z]+", FileText))  # Regex to match all words
+Number_Of_Sentences = len(re.split("[.!?]+", FileText))
+
+print(Number_Of_Words)
+print(Number_Of_Sentences)
