24ebd64e5aa51e13424e8c2f63f58c118a4c7ad9
anushab05 <anusha50898@gmail.com>
2018-05-02 00:25:53 +0530

    Initial commit

---
+++ SyllableCounter.py
@@ -0,0 +1,83 @@
+
+
+def syllables(word):
+    word = word.lower()
+    saved = word;
+    word = word +" "  # word extended
+    length = len(word);
+    ending = ["ing " ,"ed " ,"es " ,"ous " ,"tion" ,"nce" ,"ness"]  # not included in complex words
+
+    for end in ending:
+        x = word.find(end)
+        if x > -1:
+            x=length-x
+            word = word[:-x]
+    vowels = "aeiouy"
+    syllableCount = 0
+    if word[-1] == " ":
+        word = word[:-1]
+    # removing the extra " " at the end if failed and dropping last letter if e
+    if word[-1] == "e":
+        if word[-3:] == ("nce" or "rce"):
+            syllableCount = 0
+        elif word[-3] and word[-2] not in vowels and word[-3:] != ("nce" or "rce"):
+            syllableCount+=1  # e cannot be dropped as it contributes to a syllable
+        word = word[:-1]
+
+    TwoSyllables = ["ao","uo","ia","e o","e a","u u","e ous", "uou" ,"ii"," io","u a"]
+    OneSyllable = ["ie","ai","ae","a u","a a","e e","e i","e u","i u","o a","o e","o i","u e","u i","u o","o o"]
+    LastLetter = str()
+
+    # last letter is null for the first alphabet
+    for index, alphabet in enumerate(word):
+        if alphabet in vowels :
+            CurrentCombo = LastLetter+alphabet
+            if len(CurrentCombo) == 1 :  # if it's the first alphabet
+                if word[1] not in vowels:  # followed by a consonant, then one syllable
+                    syllableCount += 1
+
+                    LastLetter = word[1]
+                else:
+                    syllableCount += 1  # followed by a vowel
+                    LastLetter = alphabet
+
+            else :
+                if CurrentCombo in TwoSyllables :
+                    syllableCount+=1  # vowel combination forming 2 syllable
+
+                    LastLetter = alphabet
+                else :  # two vowels vowel as well as non vowel combination
+                    if CurrentCombo in OneSyllable:
+                        if CurrentCombo + word[index+1] == word[- 3 :] :  # ier conributes to 2 syllables
+
+                            syllableCount+=1
+                        LastLetter = alphabet
+
+
+                    else:
+
+                        if LastLetter not in vowels:
+                            syllableCount+=1
+                            LastLetter = alphabet
+
+                        else:
+
+                            # not in vowel combination
+                            LastLetter = alphabet
+
+
+        else :
+            LastLetter=alphabet
+
+
+
+    if word[-2:]  ==  "sm" : syllableCount+=1
+
+
+    if syllableCount == 0:
+        syllableCount += 1
+    print("Number of syllables in ",saved," is ",syllableCount)
+
+
+word = input("Enter a string\n")
+syllables(word)
