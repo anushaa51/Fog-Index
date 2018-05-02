3f7bcd56f507e05e6a7e5c8d8c7fa502d51443f6
anushab05 <anusha50898@gmail.com>
2018-05-02 21:54:14 +0530

    y vowel combo at the beginning

--- SyllableCounter.py
+++ SyllableCounter.py
@@ -4,7 +4,7 @@ def syllables(word):
     word = word.lower()
     saved = word
     word = word + " "  # word extended
-    length = len(word);
+    length = len(word)
     ending = ["ing " ,"ed " ,"es " ,"ous " ,"tion " ,"nce " ,"ness ","ly "]  # not included in complex words
 
     for end in ending:
@@ -25,9 +25,9 @@ def syllables(word):
 
                 syllableCount += 1  # e cannot be dropped as it contributes to a syllable
         word = word[:-1]
-
-    TwoSyllables = ["ao","uo","ia","eo","ea","uu","eous", "uou" ,"ii"," io","ua","ya"]
-    OneSyllable = ["ie","ai","ae","au","aa","ee","ei","eu","iu","oa","oe","oi","ue","ui","uo","oo","yo","yi","ye","yu","ay"]
+    one_syllable_beg = ["ya", "ae", "oe","ea","yo","yu","ye"]
+    TwoSyllables = ["ao","uo","ia","eo","ea","uu","eous", "uou" ,"ii","io","ua","ya","yo","yu","ye"]
+    OneSyllable = ["ie","ai","ae","au","aa","ee","ei","eu","iu","oa","oe","oi","ue","ui","uo","oo","yi","ay"]
     LastLetter = str()
     # last letter is null for the first alphabet
     for index, alphabet in enumerate(word):
@@ -43,13 +43,17 @@ def syllables(word):
                     LastLetter = alphabet
 
             else :
-                if CurrentCombo in TwoSyllables and CurrentCombo != word[:2] :
-                    syllableCount += 1  # vowel combination forming 2 syllable
+                if CurrentCombo in TwoSyllables :
+                    #if they're only 1 syllable at the beginning of a word, don't increment
+                    if CurrentCombo == word[:2] and CurrentCombo in one_syllable_beg :
+                        syllableCount+=0
+                    else :
+                        syllableCount += 1  # vowel combination forming 2 syllable
 
                     LastLetter = alphabet
                 else :  # two vowels vowel as well as non vowel combination
                     if CurrentCombo in OneSyllable:
-                        if CurrentCombo + word[index+1] == word[- 3 :] :  # ier conributes to 2 syllables
+                        if CurrentCombo + word[index+1] == word[- 3:] :  # ier conributes to 2 syllables
 
                             syllableCount+=1
                         LastLetter = alphabet
