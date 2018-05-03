5e2e4cd3986ee880906714f668e657fe2883873e
anushab05 <anusha50898@gmail.com>
2018-05-03 22:15:55 +0530

    minor modifications

--- FogCalc.py
+++ FogCalc.py
@@ -2,19 +2,19 @@ from WSCount import wordcount, sentencecount, counting
 
 
 def main():
-    try :
+    try:
         fog_index_calculated = ((wordcount()/sentencecount()) + counting())*0.4
         gunning_fog_index = ((wordcount()/sentencecount()) + 100*(counting()/wordcount()))*0.4
-    except ZeroDivisionError :
+    except ZeroDivisionError:
         fog_index_calculated = gunning_fog_index = 0
-    new_file = open("FogIndex.txt", "w+")
 
-    new_file.write("The Fog Index of the given text document is " + str(fog_index_calculated) + "\n")
-    new_file.write("The Gunning Fog Index of the given document is " + str(gunning_fog_index)+"\n")
-    new_file.write("Total number of sentences = " + str(sentencecount())+"\n")
-    new_file.write("Total number of words = " + str(wordcount()) + "\n")
-    new_file.write("Total number of words with 3 or more syllables =" + str(counting()) + "\n")
-    new_file.close()
+    with open("FogIndex.txt", "w+") as new_file:
+
+        new_file.write("The Fog Index of the given text document is " + str(fog_index_calculated) + "\n")
+        new_file.write("The Gunning Fog Index of the given document is " + str(gunning_fog_index)+"\n")
+        new_file.write("Total number of sentences = " + str(sentencecount())+"\n")
+        new_file.write("Total number of words = " + str(wordcount()) + "\n")
+        new_file.write("Total number of words with 3 or more syllables = " + str(counting()) + "\n")
 
 
 
--- WSCount.py
+++ WSCount.py
@@ -6,9 +6,10 @@ from CompoundWord import split
 
 try:
     FileObject = open("TestDocument.txt", "r")
-except FileNotFoundError :
-    with open("FileNotFoundError.txt", "w+") as new_file:
-        new_file.write("File does not exist, please try again by placing a valid file named 'TestDocument.txt' in current directory")
+except FileNotFoundError:
+    with open("FileNotFound.txt", "w+") as new_file:
+        new_file.write("File does not exist, please try again by placing a valid file named 'TestDocument.txt' in "
+                       "current directory")
     sys.exit(0)
 
 file_contents = FileObject.read()
@@ -17,11 +18,9 @@ file_contents = FileObject.read()
 def counting():
 
     syllablecount = 0
-
     beg_each_Sentence = re.findall(r"\.\s*(\w+)", file_contents)
-    capital_words = re.findall(r'\b[A-Z][a-z]+\b', file_contents)
+    capital_words = re.findall(r"\b[A-Z][a-z]+\b", file_contents)
     words = file_contents.split()
-    print(file_contents)
     for word in words:
         if word not in capital_words and len(word) >= 3: #all lower case words
 
@@ -34,9 +33,11 @@ def counting():
                 syllablecount += 1
     return syllablecount
 
+
 def wordcount():
     # Regex to match all words, hyphenated words count as a compound words
     return len(re.findall("[a-zA-Z-]+", file_contents))
+
 
 def sentencecount():
     #regex to count sentences, can end with a period, "?" or "!"
