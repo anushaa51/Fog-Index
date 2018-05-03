e0d449aa75f30b49c72d2146692f74c0ea0a7077
anushab05 <anusha50898@gmail.com>
2018-05-03 21:50:48 +0530

    WScount redundancy removed

--- WSCount.py
+++ WSCount.py
@@ -7,7 +7,8 @@ from CompoundWord import split
 try:
     FileObject = open("TestDocument.txt", "r")
 except FileNotFoundError :
-    print("File does not exist, please try again by placing a valid file named 'TestDocument.txt' in current directory")
+    with open("FileNotFoundError.txt", "w+") as new_file:
+        new_file.write("File does not exist, please try again by placing a valid file named 'TestDocument.txt' in current directory")
     sys.exit(0)
 
 file_contents = FileObject.read()
@@ -19,8 +20,8 @@ def counting():
 
     beg_each_Sentence = re.findall(r"\.\s*(\w+)", file_contents)
     capital_words = re.findall(r'\b[A-Z][a-z]+\b', file_contents)
-    with open("TestDocument.txt", "r") as f:
-        words = f.read().split()
+    words = file_contents.split()
+    print(file_contents)
     for word in words:
         if word not in capital_words and len(word) >= 3: #all lower case words
 
@@ -33,11 +34,9 @@ def counting():
                 syllablecount += 1
     return syllablecount
 
-
 def wordcount():
     # Regex to match all words, hyphenated words count as a compound words
     return len(re.findall("[a-zA-Z-]+", file_contents))
-
 
 def sentencecount():
     #regex to count sentences, can end with a period, "?" or "!"
